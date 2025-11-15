69faac3a98f9ef7d627bdedfc3dda1a5081253b9
Xiaoming Xi <xiaomingxi@sifli.com>
2025-11-15 20:12:31 +0800

    [int][opt][misc] Delete unused files
    Project: SiFliSDK
    
    ext-redmine: REDMINE-101
    
    [Description in detail]
    
    Affected branch: [main]

--- drivers/cmsis/sf32lb56x/sdhci.h
+++ drivers/cmsis/sf32lb56x/sdhci.h
@@ -1,26 +1,12 @@
 /*
- *  linux/drivers/mmc/host/sdhci.h - Secure Digital Host Controller Interface driver
+ * SPDX-FileCopyrightText: 2019-2025 SiFli Technologies(Nanjing) Co., Ltd
  *
- *  Copyright (C) 2005-2008 Pierre Ossman, All Rights Reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
+ * SPDX-License-Identifier: Apache-2.0
  */
+
+
 #ifndef __SDHCI_H
 #define __SDHCI_H
-
-//#include <linux/scatterlist.h>
-//#include <linux/compiler.h>
-//#include <linux/types.h>
-//#include <linux/io.h>
-//#include <host.h>
-
-//#include "/home/SD3.0/mmc/test/test.h"
-/*
- * Controller registers
- */
 
 #define SDHCI_DMA_ADDRESS    0x00
 
